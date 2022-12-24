284bf085187e40419bffa05d2a76bf7622301bea
OfficialB <reconispog@gmail.com>
2022-12-23 17:18:48 -0800

    Add product information

--- server.go
+++ server.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+)
+
+var (
+	version = "1.0.0"
+	author  = "Official B"
 )
 
 func main() {
 	router := mux.NewRouter()
 
 	fs := http.FileServer(http.Dir("html"))
+	fmt.Print("Hello, World! The current version of gowebserver is " + version + " and it was written by " + author + ".\n-----------------------------\n")
+	fmt.Print("To exit the program, enter the key combination \"CTRL + C\".\n")
 	router.PathPrefix("/").Handler(http.StripPrefix("/", fs))
 
 	http.ListenAndServe(":8080", router)
