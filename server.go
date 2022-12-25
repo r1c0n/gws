777e6254d8e75f9baf793a5e3e2a7fa196554147
OfficialB <reconispog@gmail.com>
2022-12-24 21:55:44 -0800

    Add comments to server.go for better understanding of code

--- server.go
+++ server.go
@@ -12,43 +12,53 @@ import (
 )
 
 type Config struct {
-	Port      string `json:"port"`
-	StaticDir string `json:"static_dir"`
+	Port      string `json:"port"`       // Port is the port number that the server will listen on.
+	StaticDir string `json:"static_dir"` // StaticDir is the directory where static assets (e.g. HTML, CSS, JavaScript) are stored.
 
+	// TLSConfig contains configuration options for TLS (Transport Layer Security).
 	TLSConfig struct {
-		CertFile string `json:"cert_file"`
-		KeyFile  string `json:"key_file"`
+		CertFile string `json:"cert_file"` // CertFile is the path to the TLS certificate file.
+		KeyFile  string `json:"key_file"`  // KeyFile is the path to the TLS key file.
 	} `json:"tls_config"`
 
+	// RepoConfig contains information about the repository.
 	RepoConfig struct {
-		Version    string `json:"version"`
-		Author     string `json:"author"`
-		Product    string `json:"product"`
-		Repository string `json:"repository"`
+		Version    string `json:"version"`    // Version is the version of the repository.
+		Author     string `json:"author"`     // Author is the author of the repository.
+		Product    string `json:"product"`    // Product is the name of the product that the repository is for.
+		Repository string `json:"repository"` // Repository is the name of the repository.
 	} `json:"repo_config"`
 }
 
 func main() {
+	// Read the configuration file
 	configData, err := ioutil.ReadFile("config.json")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) // If there is an error reading the file, log the error and exit the program.
 	}
 
+	// Parse the configuration data into a Config struct.
 	var config Config
 	if err := json.Unmarshal(configData, &config); err != nil {
-		log.Fatal(err)
+		log.Fatal(err) // If there is an error parsing the data, log the error and exit the program.
 	}
 
+	// Clear the screen.
 	screen.Clear()
 
+	// Print a message coontaining information from the Config struct. Mostly repository information.
 	fmt.Print("Hello, World! | ", config.RepoConfig.Product, " v", config.RepoConfig.Version, " | Created by ", config.RepoConfig.Author)
 	fmt.Print("\nTo contribute, check out our GitHub repo: ", config.RepoConfig.Repository, ".")
 	fmt.Print("\n----------------------------------------------------------------------------\n")
 	fmt.Print("To exit the program, enter the key combination \"CTRL + C\".\n")
 	fmt.Print("Site URL: http://localhost", config.Port, "\n")
 
-	router := mux.NewRouter()
+	router := mux.NewRouter() // Create a new router.
+
+	// Set up a FileServer to serve static assets.
 	fs := http.FileServer(http.Dir(config.StaticDir))
 	router.PathPrefix("/").Handler(http.StripPrefix("/", fs))
+
+	// Start the server.
 	http.ListenAndServe(config.Port, router)
 }
